d43857bde09ada5e292ede68c491d40874e9ddf8
Oluwatobi <oladipupo_tobi@yahoo.com>
2019-12-02 20:13:28 +0100

    Initial Commit

---
+++ JobApps/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JobApps
+//
+//  Created by user on 02/12/2019.
+//  Copyright © 2019 Oladipupo Oluwatobi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
